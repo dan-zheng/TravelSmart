f8453ed6ccf59b09b2241f77699dce67c5318a24
Dan Zheng <zheng321@purdue.edu>
2016-09-24 23:20:53 -0400

    Change initial location/zoom of map

--- public/js/main.js
+++ public/js/main.js
@@ -14,8 +14,8 @@ function initMap() {
     var directionsService = new google.maps.DirectionsService;
     var directionsDisplay = new google.maps.DirectionsRenderer;
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 7,
-        center: {lat: 41.85, lng: -87.65}
+        zoom: 14,
+        center: {lat: 40.4237095, lng: -86.9233833}
     });
     directionsDisplay.setMap(map);
 
