1f3b4c298cbddc38c089de68608ce2f04f760848
Dan Zheng <zheng321@purdue.edu>
2016-09-24 20:09:36 -0400

    Remove unnecessary dependencies

--- app.js
+++ app.js
@@ -3,23 +3,13 @@
  */
 const express = require('express');
 const compression = require('compression');
-const session = require('express-session');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const errorHandler = require('errorhandler');
-const lusca = require('lusca');
-const dotenv = require('dotenv');
-const MongoStore = require('connect-mongo')(session);
 const flash = require('express-flash');
 const path = require('path');
-const mongoose = require('mongoose');
 const passport = require('passport');
 const sass = require('node-sass-middleware');
-
-/**
- * Load environment variables from .env file, where API keys and passwords are configured.
- */
-dotenv.load({ path: '.env' });
 
 /**
  * Controllers (route handlers).
@@ -45,38 +35,6 @@ app.use(sass({
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-/*app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: process.env.SESSION_SECRET,
-    store: new MongoStore({
-        url: process.env.MONGOLAB_URI || process.env.MONGODB_URI,
-        autoReconnect: true
-    })
-}));*/
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-app.use((req, res, next) => {
-    lusca.csrf()(req, res, next);
-});
-app.use(lusca.xframe('SAMEORIGIN'));
-app.use(lusca.xssProtection(true));
-app.use((req, res, next) => {
-    res.locals.user = req.user;
-    next();
-});
-app.use(function(req, res, next) {
-    // After successful login, redirect back to the intended page
-    if (!req.user &&
-            req.path !== '/login' &&
-            req.path !== '/signup' &&
-            !req.path.match(/^\/auth/) &&
-            !req.path.match(/\./)) {
-        req.session.returnTo = req.path;
-    }
-    next();
-});
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
 /**
