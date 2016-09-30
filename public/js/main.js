77a15c1b525e2d1c211ceed83ce70d28dd6c3fb6
Dan Zheng <zheng321@purdue.edu>
2016-09-29 22:36:38 -0400

    Update new app features

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,110 @@
 $(document).ready(function() {
     var height = $(window).height() - $(".navbar").height() - $("footer").height();
     $("#map").height(height);
-    $("#map").css("padding-top", $(".navbar").height());
+    $("#map").css("margin-top", $(".navbar").height());
 });
 
 $(window).on('resize', function() {
-    var height = $(window).height() - $(".navbar").height() * 2 - $("footer").height();
+    var height = $(window).height() - $(".navbar").height() - $("footer").height();
     $("#map").height(height);
-    $("#map").css("padding-top", $(".navbar").height());
+    $("#map").css("margin-top", $(".navbar").height());
 });
 
 function initMap() {
-    var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer;
+	var origin_place_id = null;
+	var destination_place_id = null;
+	var travel_mode = 'WALKING';
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 14,
-        center: {lat: 40.4237095, lng: -86.9233833}
-    });
-    directionsDisplay.setMap(map);
-
-    var onChangeHandler = function() {
-        calculateAndDisplayRoute(directionsService, directionsDisplay);
-    };
-    document.getElementById('start').addEventListener('change', onChangeHandler);
-    document.getElementById('end').addEventListener('change', onChangeHandler);
-  }
-
-  function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-    directionsService.route({
-        origin: document.getElementById('start').value,
-        destination: document.getElementById('end').value,
-        travelMode: 'DRIVING'
-    }, function(response, status) {
-        if (status === 'OK') {
-            directionsDisplay.setDirections(response);
-        } else {
-            window.alert('Directions request failed due to ' + status);
+        center: {
+            lat: 40.4237095,
+            lng: -86.9233833
         }
     });
+	var directionsService = new google.maps.DirectionsService;
+	var directionsDisplay = new google.maps.DirectionsRenderer;
+	directionsDisplay.setMap(map);
+
+	var origin_input = document.getElementById('origin-input');
+	var destination_input = document.getElementById('destination-input');
+	var modes = document.getElementById('mode-selector');
+
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(origin_input);
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(destination_input);
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(modes);
+
+	var origin_autocomplete = new google.maps.places.Autocomplete(origin_input);
+	origin_autocomplete.bindTo('bounds', map);
+	var destination_autocomplete =
+		new google.maps.places.Autocomplete(destination_input);
+	destination_autocomplete.bindTo('bounds', map);
+
+	// Sets a listener on a radio button to change the filter type on Places
+	// Autocomplete.
+	function setupClickListener(id, mode) {
+		var radioButton = document.getElementById(id);
+		radioButton.addEventListener('click', function() {
+			travel_mode = mode;
+		});
+	}
+	setupClickListener('changemode-walking', 'WALKING');
+	setupClickListener('changemode-transit', 'TRANSIT');
+	setupClickListener('changemode-driving', 'DRIVING');
+
+	function expandViewportToFitPlace(map, place) {
+		if (place.geometry.viewport) {
+			map.fitBounds(place.geometry.viewport);
+		} else {
+			map.setCenter(place.geometry.location);
+			map.setZoom(17);
+		}
+	}
+
+	origin_autocomplete.addListener('place_changed', function() {
+		var place = origin_autocomplete.getPlace();
+		if (!place.geometry) {
+			window.alert("Autocomplete's returned place contains no geometry");
+			return;
+		}
+		expandViewportToFitPlace(map, place);
+
+		// If the place has a geometry, store its place ID and route if we have
+		// the other place ID
+		origin_place_id = place.place_id;
+		route(origin_place_id, destination_place_id, travel_mode,
+			directionsService, directionsDisplay);
+	});
+
+	destination_autocomplete.addListener('place_changed', function() {
+		var place = destination_autocomplete.getPlace();
+		if (!place.geometry) {
+			window.alert("Autocomplete's returned place contains no geometry");
+			return;
+		}
+		expandViewportToFitPlace(map, place);
+
+		// If the place has a geometry, store its place ID and route if we have
+		// the other place ID
+		destination_place_id = place.place_id;
+		route(origin_place_id, destination_place_id, travel_mode,
+			directionsService, directionsDisplay);
+	});
+
+	function route(origin_place_id, destination_place_id, travel_mode,
+		directionsService, directionsDisplay) {
+			if (!origin_place_id || !destination_place_id) {
+				return;
+			}
+			directionsService.route({
+				origin: {'placeId': origin_place_id},
+				destination: {'placeId': destination_place_id},
+				travelMode: travel_mode
+			}, function(response, status) {
+				if (status === 'OK') {
+					directionsDisplay.setDirections(response);
+				} else {
+					window.alert('Directions request failed due to ' + status);
+				}
+			});
+		}
 }
